8250de46ae692dccf724a22947b8d42a875eae1d
lemmyMwaura <lemmymwauracodes@gmail.com>
2024-07-26 22:55:57 +0300

    docs: update README.md

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -1,12 +1,14 @@
 export const Bio = () => {
   return (
     <>
-      <div className="flex flex-col gap-3">
-        <div className="font-bold text-3xl fancy">Stephen Mwaura</div>
-        <div>
+      <div className="flex flex-col gap-3 p-9">
+        <div className="flex w-full font-bold text-6xl fancy items-center">
+          Stephen Lemmy Mwaura
+        </div>
+        {/* <div>
           I&apos;m a Software Engineer, Typescriptee, Open Source enthusiast and
           contributor living and learning in Nairobi kenya.
-        </div>
+        </div> */}
       </div>
     </>
   )
