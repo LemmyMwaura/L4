e667e214707040134d640ef97f8810e042d2f793
lemmyMwaura <lemminton@gmail.com>
2024-09-16 20:31:08 +0300

    feat: subtle updates to bio

--- src/components/Bio.tsx
+++ src/components/Bio.tsx
@@ -4,8 +4,8 @@ export const Bio = () => {
       <div className="flex flex-col gap-3">
         <div className="font-bold text-3xl fancy">Stephen Mwaura</div>
         <div>
-          I&apos;m a Software Engineer, Typescriptee, Open Source enthusiast and
-          contributor living and learning in Nairobi kenya.
+          I&apos;m a Software Engineer, TypeScript enthusiast, open-source
+          contributor, living and learning in Nairobi, Kenya.
         </div>
       </div>
     </>
